8082e0c812c12411939b0558f8b45e17900db9f9
aditseller <tezaraditya@gmail.com>
2016-02-07 15:18:43 +0700

    Share Button

--- views/career/detail.php
+++ views/career/detail.php
@@ -140,3 +140,7 @@ $checkSendCV = \app\models\Sendcv::find()->where(['id_composite'=>$model->id_car
 
 
 </div>
+
+
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-56b6f9f045bd533a" async="async"></script>
