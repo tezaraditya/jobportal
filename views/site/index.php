9d6c07edc094d103e3dffbc914f21506d7a70ade
aditseller <tezaraditya@gmail.com>
2016-02-08 20:14:24 +0700

    redesign function and location

--- views/site/index.php
+++ views/site/index.php
@@ -38,16 +38,16 @@ $this->title = 'Situs Lowongan Kerja dan CV Online Terkini - '.Yii::$app->params
 
 
 <div class="container">
-<div class="panel panel-info">
-<div class="panel-heading">
+<div class="panel">
+
   <!-- Nav tabs -->
-  <ul class="nav nav-tabs" role="tablist">
-    <li role="presentation" class="active"><a href="#JobFunction" aria-controls="JobFunction" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-briefcase"></span> Job Function</a></li>
-    <li role="presentation"><a href="#Location" aria-controls="Location" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-map-marker"></span> Location</a></li>
+  <ul class="nav nav-pills" role="tablist">
+    <li role="presentation" class="active"><a href="#JobFunction" aria-controls="JobFunction" role="tab" data-toggle="tab"><h4><span class="glyphicon glyphicon-briefcase"></span> Job Function</h4></a></li>
+    <li role="presentation"><a href="#Location" aria-controls="Location" role="tab" data-toggle="tab"><h4><span class="glyphicon glyphicon-map-marker"></span> Location</h4></a></li>
 
   </ul>
-
 </div>
+<div class="panel  panel-default">
 
 <div class="panel-body">
 
@@ -59,10 +59,10 @@ $this->title = 'Situs Lowongan Kerja dan CV Online Terkini - '.Yii::$app->params
       //list Job Function in Index
        $JobFunction = \app\models\JobFunction::find()->orderBy('id_job_function')->all();
        foreach($JobFunction as $row) {
-           echo "<div class='col-lg-3 style:height:0px;'>"
-           .Html::a($row->function,['career/index','CareerSearch[function]'=>$row->function]).
-          " (".\app\models\Career::find()->where(['function'=>$row->function])->count().
-           ")</div>";
+           echo "<div class='col-lg-3 style:height:0px;'><div class='list-group'>"
+           .Html::a('<button type="button" class="list-group-item"><b>'.$row->function.'</b></button>',['career/index','CareerSearch[function]'=>$row->function]).
+          //" (".\app\models\Career::find()->where(['function'=>$row->function])->count().")".
+           "</div></div>";
        }
 
     ?>
@@ -76,10 +76,10 @@ $this->title = 'Situs Lowongan Kerja dan CV Online Terkini - '.Yii::$app->params
       //list Location in Index
        $Location = app\models\Location::find()->orderBy('id_location')->all();
        foreach($Location as $row) {
-           echo "<div class='col-lg-3 style:height:0px;'><h3>"
-           .Html::a($row->location,['career/index','CareerSearch[location]'=>$row->location]).
-           " (".app\models\Career::find()->where(['location'=>$row->location])->count().
-           ")</h3></div>";
+           echo "<div class='col-lg-3 style:height:0px;'><div class='list-group'>"
+           .Html::a('<button type="button" class="list-group-item"><b><span class="glyphicon glyphicon-map-marker"></span> '.$row->location.'</b></button>',['career/index','CareerSearch[location]'=>$row->location]).
+           //" (".app\models\Career::find()->where(['location'=>$row->location])->count().")".
+           "</div></div>";
        }
 
     ?>
