dbd91501d1ad161ed436b6c85c1bc74e19aefe29
aditseller <tezaraditya@gmail.com>
2016-03-27 02:54:06 +0700

    params

--- config/params.php
+++ config/params.php
@@ -4,6 +4,6 @@ return [
     'adminEmail' => 'admin@example.com',
 	  'siteName' => 'Resumeditor.com',
     'employersUrl'=>'http://company.resumeditor.com/',
-    'homeUrl' => 'http://resumeditor.com',
+
 
 ];
