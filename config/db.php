1607b7909d5b6482da11cbe8503c1470fa956c4c
aditseller <tezaraditya@gmail.com>
2016-06-17 13:19:50 +0700

    blablabla

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=resumeditor',
     'username' => 'root',
-    'password' => '',
+    'password' => 'toor',
     'charset' => 'utf8',
 ];
